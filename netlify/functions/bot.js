858049f61390c331a8f240d8ea16e64839870c43
Danil <danil.shtorg111@mail.ru>
2024-11-22 02:42:22 +0700

    Добавил WebApp-кнопку в бота

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,8 +1,25 @@
 const { Telegraf } = require('telegraf');
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Обработчик текстовых сообщений
 bot.on('text', (ctx) => {
-  ctx.reply('Привет! Я получил твоё сообщение.');
+  // URL вашего веб-приложения
+  const webAppUrl = 'https://durak332.netlify.app';
+
+  // Отправляем сообщение с кнопкой WebApp
+  ctx.reply('Открыть приложение:', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Перейти в WebApp',
+            web_app: { url: webAppUrl }, // Ссылка на WebApp
+          },
+        ],
+      ],
+    },
+  });
 });
 
 exports.handler = async (event) => {
