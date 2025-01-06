1068bb06bdfd8c66766a6a16302bd97a037623d3
Mitrios19 <sapronov.dmitrii19@yandex.com>
2025-01-06 19:16:49 +0000

    Test push to trigger GitHub Actions

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const handle = app.getRequestHandler(); // Обработчик для Next.js
   });
 
   server.get('/nikita', (req, res) => {
-    res.send('Никита');
+    res.send('11Никита11');
   });
 
   server.get('/danil', (req, res) => {
