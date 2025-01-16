6c970d6037309b62578052bb25ddaf944e4e9d45
danil3321 <160486997+danil3321@users.noreply.github.com>
2025-01-16 17:39:21 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
             </svg>
           </div>
           <div className="ml-3">
-            <p className="text-sm font-semibold">Изменения внес через редактор в гите</p>
+            <p className="text-sm font-semibold">Йооу</p>
             <p className="text-xs text-gray-400">bronze 1/10</p>
           </div>
         </div>
