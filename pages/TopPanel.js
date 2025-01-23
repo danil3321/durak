a1c5f19ff390fbf220b109f24535e41cb8f55211
Mitrios19 <99819966+Mitrios19@users.noreply.github.com>
2025-01-23 13:14:13 +0000

    Замена шрифта и расположения

--- pages/TopPanel.js
+++ pages/TopPanel.js
@@ -3,8 +3,8 @@ export default function TopPanel() {
     <div className="w-full bg-black py-4 px-4 flex items-center justify-center rounded-b-[15px] border-b border-white">
       {/* Центрированный контент */}
       <div className="text-center">
-        <p className="text-[24px] font-light text-[rgb(215,220,224)] leading-[0px] tracking-[0px] font-quicksand">Mitrios</p>
-        <p className="text-[16px] font-light text-[rgb(215,220,224)] leading-[20px] tracking-[0px] font-quicksand">bronze 1/10</p>
+        <p className="text-[24px] font-light text-[rgb(215,220,224)] leading-[0px] tracking-[0px] font-quicksand mb-1">Mitrios</p>
+        <p className="text-[16px] font-light text-[rgb(215,220,224)] leading-[20px] tracking-[0px] font-quicksand mt-1">bronze 1/10</p>
       </div>
     </div>
   );
